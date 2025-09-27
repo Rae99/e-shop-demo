553e7db1ae60db94d80072d07963d317fae29cfd
Rae99 <ruibantian812@gmail.com>
2025-09-27 13:20:06 -0700

    <Header>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,16 @@
-import React from 'react'
+import { Container } from 'react-bootstrap'
+import Header from './components/Header'
 
 const App = () => {
   return (
-    <div>
-      <h1>Welcome to the E-Shop</h1>
-    </div>
+    <>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <h1>Welcome to E-Shop</h1>
+        </Container>
+      </main>
+    </>
   )
 }
 
