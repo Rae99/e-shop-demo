880751a23d36c37f310091e4daf9902e0d2b20a7
Rae99 <ruibantian812@gmail.com>
2025-12-12 15:56:25 -0800

    details page built

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,32 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider}from 'react-router-dom'
-
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom';
 
 import App from './App';
 // import 'bootstrap/dist/css/bootstrap.min.css';
 import reportWebVitals from './reportWebVitals';
-import "./assets/styles/bootstrap.custom.css"
-import"./assets/styles/index.css"
+import './assets/styles/bootstrap.custom.css';
+import './assets/styles/index.css';
 import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/*" element={<App />}>
-      <Route index element={<HomeScreen />} /> 
+    <Route path="/" element={<App />}>
+      <Route index element={<HomeScreen />} />
       {/* index, or index={true} works the same */}
-
-
+      <Route path="product/:id" element={<ProductScreen />} />
     </Route>
   )
-)
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render( 
+root.render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>
 );
-
 
 reportWebVitals();
