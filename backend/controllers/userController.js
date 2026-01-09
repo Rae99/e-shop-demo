9226d218ec1805317eaf49f65f628bcd7f3f1771
Rae99 <ruibantian812@gmail.com>
2026-01-09 14:25:08 -0800

    user register and encryption

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,7 @@
 import User from '../models/userModel.js';
 import asyncHandler from '../middleware/asyncHandler.js';
 import jwt from 'jsonwebtoken';
+import generateToken from '../utils/generateToken.js';
 
 // @desc    Authenticate user & get token
 // @route   POST /api/users/login
@@ -11,18 +12,7 @@ const authUser = asyncHandler(async (req, res) => {
   const user = await User.findOne({ email });
 
   if (user && (await user.matchPassword(password))) {
-    const token = jwt.sign({ userid: user._id }, process.env.JWT_SECRET, {
-      expiresIn: '30d',
-    });
-
-    // Set JWT as HTTP-only cookie
-    res.cookie('jwt', token, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
-      sameSite: 'strict',
-      maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
-    }
-    );
+    generateToken(res, user._id); 
       
     res.json({
       _id: user._id,
@@ -41,6 +31,31 @@ const authUser = asyncHandler(async (req, res) => {
 // @route   POST /api/users/register
 // @access  Public
 const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+  
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error('User already exists');
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  if (user) {
+    generateToken(res, user._id);
+    
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  }
   res.send('Register user');
 });
 
@@ -48,7 +63,11 @@ const registerUser = asyncHandler(async (req, res) => {
 // @route POST /api/users/logout
 // @access Public
 const logoutUser = asyncHandler(async (req, res) => {
-  res.send('Logout user');
+  res.cookie('jwt', '', {
+    httpOnly: true,
+    expires: new Date(0), // clear the cookie
+  });
+  res.status(200).json({ message: 'Logged out successfully' });
 });
 
 // @desc    Get user profile
